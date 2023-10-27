ed6d291d88cc907c32354650d82201666bf80143
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2023-10-27 11:44:02 -0400

    lulz

--- chapter1/chessBoard.js
+++ chapter1/chessBoard.js
@@ -1,5 +1,23 @@
-// official solution
+// my solution
+
 let size = 8;
+
+let bord = "";
+
+for (let x = 0; x < size; x++) {
+  for (let y = 0; y <= size; y++) {
+    if ((x + y) % 2 == 0) {
+      bord += " ";
+    } else {
+      bord += "#";
+    }
+  }
+  bord += "\n";
+}
+
+console.log(bord);
+
+// official solution
 
 let board = "";
 
