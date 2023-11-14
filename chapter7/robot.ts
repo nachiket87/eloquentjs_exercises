ec8d5693e6054b95381dc704d423658b6084066f
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2023-11-14 13:41:05 -0500

    startrobot

---
+++ chapter7/robot.ts
@@ -0,0 +1,65 @@
+const roads: string[] = [
+  "Alice's House-Bob's House",
+  "Alice's House-Cabin",
+  "Alice's House-Post Office",
+  "Bob's House-Town Hall",
+  "Daria's House-Ernie's House",
+  "Daria's House-Town Hall",
+  "Ernie's House-Grete's House",
+  "Grete's House-Farm",
+  "Grete's House-Shop",
+  "Marketplace-Farm",
+  "Marketplace-Post Office",
+  "Marketplace-Shop",
+  "Marketplace-Town Hall",
+  "Shop-Town Hall",
+];
+
+const buildGraph = (edges: string[]) => {
+  let graph = Object.create(null);
+  function addEdge(from: string, to: string) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+  for (let [from, to] of edges.map((r) => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+};
+
+const roadGraph = buildGraph(roads);
+
+interface Parcels {
+  place: string;
+  address: string;
+}
+
+class VillageState {
+  place: string;
+  parcels: Parcels[];
+
+  constructor(place: string, parcels: Parcels[]) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination: string) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels
+        .map((p) => {
+          if (p.place != this.place) return p;
+          return { place: destination, address: p.address };
+        })
+        .filter((p) => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
+
+const villageState = new VillageState("Shop", [{ place: "Town Hall", address: "Town Hall" }]);
